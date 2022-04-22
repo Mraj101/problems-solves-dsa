d3793c7539179ce3273d4cdd7f9a6cd54cfceaf4
Hasnain <mrajhasnain101@gmail.com>
2022-04-22 19:52:42 +0600

    Edited file

--- Sorting techniques/selectionSort.cpp
+++ Sorting techniques/selectionSort.cpp
@@ -8,17 +8,26 @@ void PrintArray(int arr[],int n)
     {
         printf("%d ",arr[i]);
     }
+    printf("\n");
 }
 
 
 //Sorting is done here
 void SelectionSort(int arr[],int n)
 {
-    int i=0;
+    int i;
+    int j;//variable for moving the indexes
+    int k;//variable for storing the position of minimum number
     int temp=arr[i];
     for(i=0;i<n-1;i++)
     {
-        
+        for( j=k=i;j<n;j++)
+        {
+            if(arr[j]<arr[k])
+                k=j;
+                
+        }
+        swap(arr[i],arr[k]);
     }
 
 }
@@ -28,6 +37,8 @@ int main(){
 int arr[]={10,9,8,7,6,5,4,3,2,1};
 int length=sizeof(arr)/sizeof(int); //calculating the length of array
 PrintArray(arr,length); //calling the printarray function
+SelectionSort(arr,length);
+PrintArray(arr,length); //calling the printarray function
 
 return 0;
-}
+} 
