ea115750a674c8b16e97098780ba70b1e62bd315
Hasnain <mrajhasnain101@gmail.com>
2022-06-01 21:54:49 +0600

    val

---
+++ Problems Solves cpp/uva problems/12250-LanguageDetection.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char lang[1024];
+    int c=0;
+    while (scanf("%s", lang) != EOF)
+    {
+        c++;
+       if(strcmp(lang,"#")==0)
+        return 0;
+
+        if (strcmp(lang, "HELLO") == 0)
+            printf("Case %d: ENGLISH\n",c);
+        else if (strcmp(lang, "HOLA") == 0)
+            printf("Case %d: SPANISH\n",c);
+        else if (strcmp(lang,"HALLO") == 0)
+            printf("Case %d: GERMAN\n",c);
+        else if (strcmp(lang, "BONJOUR") == 0)
+            printf("Case %d: FRENCH\n",c);
+        else if (strcmp(lang, "CIAO") == 0)
+            printf("Case %d: ITALIAN\n",c);
+        else if (strcmp(lang, "ZDRAVSTVUJTE") == 0)
+            printf("Case %d: RUSSIAN\n",c);
+    }
+
+    return 0;
+}
