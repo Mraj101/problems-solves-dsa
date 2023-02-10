b1c50bd47d57ef4d752de52b9b13c8aa8a98bb31
Hasnain <mrajhasnain101@gmail.com>
2023-02-10 23:24:01 +0600

    inputOutpu Cpp

---
+++ Problems Solves cpp/codeforces/fastIO.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,x;
+    cin>>n>>x;
+    cout<<"output is " <<n <<endl;
+    cout<<"output is " <<n <<"\n";
+    cout<<"output is " <<n <<endl;
+    return 0;
+}
