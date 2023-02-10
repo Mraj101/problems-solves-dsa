f2b68500a728016baca87c6c408cf1ddf8dbf0ba
Hasnain <mrajhasnain101@gmail.com>
2023-02-10 23:53:26 +0600

    define

---
+++ Problems Solves cpp/codeforces/define.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define hello printf("Hello world");
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+     
+     hello
+
+    return 0;
+}
